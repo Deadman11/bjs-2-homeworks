48e992be89c3b56b96d633bdf08b71aefb1b7ca4
Vitaliy <ilol2021@yandex.ru>
2025-04-24 16:39:36 +0300

    Готовое задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if(this.hasOwnProperty(`marks`) === true){
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(this.hasOwnProperty(`marks`) === false || this.marks.length === 0){
+    return 0;
+  }
+
+  const averageSum = this.marks.reduce((acc, mark) => acc + mark,0);
+  const gradeAverage = averageSum / this.marks.length
+
+  return gradeAverage
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }

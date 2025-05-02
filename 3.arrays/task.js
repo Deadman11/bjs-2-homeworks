93ed62e1528e26312d820491431e68caebcf5577
Vitaliy <vindette2021@yandex.ru>
2025-05-02 13:25:41 +0300

    Готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length == arr2.length && arr1.every((currentElement, index) => arr2[index] === currentElement)
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  if(users.length === 0 || gender != "мужской" && gender != "женский"){
+    return 0
+  }
+  return users.filter((gndr) => gndr.gender === gender).map((num) => num.age).reduce((acc, currentNumber)=> acc + currentNumber) / users.filter((gndr) => gndr.gender === gender).length
 }
